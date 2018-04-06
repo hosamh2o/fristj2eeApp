dd76f7f76cedbfc9d15fbf653c6c51c5540badcf
houssamaddine <32640260+hosamh2o@users.noreply.github.com>
2018-04-06 11:12:48 +0200

    Add files via upload

---
+++ firstj2ee/WebContent/WEB-INF/menu.jsp
@@ -0,0 +1,9 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+
+<ul>
+<li><a href="/firstj2ee">accueil</a></li>
+<li><a href="/firstj2ee/bonjour">bonjour</a></li>
+<li>page 3</li>
+</ul>
---
+++ firstj2ee/src/com/beans/auteur.java
@@ -0,0 +1,27 @@
+package com.beans;
+
+public class auteur {
+
+	String nom;
+	String prenom;
+	boolean actif;
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public boolean isActif() {
+		return actif;
+	}
+	public void setActif(boolean actif) {
+		this.actif = actif;
+	}
+	
+}
